6640a2a539b9781a61ab4fcd36300b1998833924
Orosz Péter <orosz401@gmail.com>
2020-02-29 11:19:11 +0100

    Delete ViewPager Use two-directional ScrollView

--- Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/activity/MainActivity.kt
+++ Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/activity/MainActivity.kt
@@ -3,20 +3,11 @@ package neptun.jxy1vz.cluedo.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import neptun.jxy1vz.cluedo.R
-import neptun.jxy1vz.cluedo.ui.activity.adapter.PagerAdapter
-import fr.castorflex.android.verticalviewpager.VerticalViewPager
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewPager: VerticalViewPager
-    private lateinit var pagerAdapter: PagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        viewPager = findViewById(R.id.viewPager)
-        pagerAdapter = PagerAdapter(supportFragmentManager)
-        viewPager.adapter = pagerAdapter
     }
 }
