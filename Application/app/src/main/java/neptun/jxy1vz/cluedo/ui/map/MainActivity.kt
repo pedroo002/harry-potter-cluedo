0d4e96e4a6acaa8e45bff6985820e5d847030732
Orosz Péter <orosz401@gmail.com>
2020-03-01 17:52:11 +0100

    Refactor code Delete unnecessary test modules (attributes, functions, ui elements)

--- Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/map/MainActivity.kt
+++ Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/map/MainActivity.kt
@@ -1,11 +1,8 @@
-package neptun.jxy1vz.cluedo.ui.activity.map
+package neptun.jxy1vz.cluedo.ui.map
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil.setContentView
 import neptun.jxy1vz.cluedo.R
 import neptun.jxy1vz.cluedo.databinding.ActivityMainBinding
@@ -18,12 +15,5 @@ class MainActivity : AppCompatActivity() {
         val greenTile = findViewById<ImageView>(R.id.ivGreenTile)
         activityMainBinding.player1 = MapViewModel(resources.displayMetrics.densityDpi, greenTile)
         activityMainBinding.executePendingBindings()
-    }
-
-    @BindingAdapter("android:layout_marginTop")
-    fun setLayoutMarginTop(view: View, margin: Float) {
-        val layoutParams: ViewGroup.MarginLayoutParams = view.layoutParams as ViewGroup.MarginLayoutParams
-        layoutParams.setMargins(layoutParams.leftMargin, margin.toInt(), layoutParams.rightMargin, layoutParams.bottomMargin)
-        view.layoutParams = layoutParams
     }
 }
