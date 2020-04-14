dbfc2e1b0e109c3056083d8711dcc878ce630d70
Orosz Péter <orosz401@gmail.com>
2020-04-14 19:04:23 +0200

    Optimize imports

--- Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/map/MapViewModel.kt
+++ Application/app/src/main/java/neptun/jxy1vz/cluedo/ui/map/MapViewModel.kt
@@ -4,7 +4,6 @@ import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.content.Context
 import android.graphics.Matrix
-import android.media.Image
 import android.view.View
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
