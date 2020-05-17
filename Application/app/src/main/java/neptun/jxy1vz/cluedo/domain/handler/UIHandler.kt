42340db9ebba30fa08887eedb93aac355b7ad032
Orosz Péter <orosz401@gmail.com>
2020-05-17 18:13:43 +0200

    Fix problem in animatePlayerWalking()

--- Application/app/src/main/java/neptun/jxy1vz/cluedo/domain/handler/UIHandler.kt
+++ Application/app/src/main/java/neptun/jxy1vz/cluedo/domain/handler/UIHandler.kt
@@ -217,8 +217,7 @@ class UIHandler(private val map: MapViewModel.Companion) : Animation.AnimationLi
             }
             path.add(0, origin)
         }
-        else {
-            //ha ajtóból indulok, nem jó
+        else if (path.size > 0) {
             val img = when (getDirection(start, path[0])) {
                 "W" -> forwardFeet[2]
                 "A" -> leftFeet[2]
