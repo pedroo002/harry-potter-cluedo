8aa403ae43f1305bebe0b3b7c1f1d42eed799028
pedroo002 <orosz401@gmail.com>
2020-09-27 00:11:31 +0200

    Add Retrofit instance Add internet dependency to manifest Create sample API service interface

---
+++ Application/app/src/main/java/neptun/jxy1vz/cluedo/api/ApiService.kt
@@ -0,0 +1,19 @@
+package neptun.jxy1vz.cluedo.api
+
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiService {
+
+    /*@POST("/login")
+    fun login(@Body body:RequestBody): Call<UserModel>
+
+    @POST("/send-message")
+    fun sendMessage(@Body body:RequestBody): Call<String>
+
+    @GET("/users")
+    fun getUsers(): Call<List<UserModel>>*/
+}
