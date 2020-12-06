868ffa55544c79635afdf4fb38750099a4e94176
pedroo002 <orosz401@gmail.com>
2020-12-06 11:06:14 +0100

    Add column infos to PlayerDBmodel

--- Application/app/src/main/java/neptun/jxy1vz/hp_cluedo/data/database/model/PlayerDBmodel.kt
+++ Application/app/src/main/java/neptun/jxy1vz/hp_cluedo/data/database/model/PlayerDBmodel.kt
@@ -1,5 +1,6 @@
 package neptun.jxy1vz.hp_cluedo.data.database.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,8 +8,8 @@ import androidx.room.PrimaryKey
 data class PlayerDBmodel(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
-    val playerName: String,
-    val playerId: Int,
-    val characterName: String,
-    val channelName: String
+    @ColumnInfo(name = "player_name") val playerName: String,
+    @ColumnInfo(name = "player_id") val playerId: Int,
+    @ColumnInfo(name = "character_name") val characterName: String,
+    @ColumnInfo(name = "channel_name") val channelName: String
 )
